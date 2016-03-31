93dd3e9f97e822338a5628637f1e257e29db5e9d
Patrick Brideau <pbrideau@kronostechnologies.com>
2016-03-31 16:10:47 -0400

    Upgrade wheelhouse to burp-ui 0.1.3

--- manifests/ui/install.pp
+++ manifests/ui/install.pp
@@ -2,52 +2,16 @@ class burp::ui::install (
   $package_provider  = 'aptitude',
   $wheelhouse_source = 'puppet:///modules/burp/ui-wheelhouse',
   $wheelhouse_path   = '/opt/burp-ui_wheelhouse',
+  $burpui_version    = '0.1.3',
 ) {
-
-  # Currently content is burp-ui 0.0.7.3
-
-  $debian_packages = [
-    'python-dateutil',
-    #'python-flask-login', Debian = 0.2.6 , require == 0.2.11
-    
-    #'python-flaskext.wtf',  Debian = 0.10.2 , require == 0.10.0
-
-    'python-wtforms',
-    'python-markupsafe',
-    #'python-flask-restful', ## Debian Stretch, no backports ##  Stretch = 0.3.4 , require == 0.3.2
-      'python-flask',
-        'python-itsdangerous',
-        'python-werkzeug',
-        'python-jinja2',
-      'python-six',
-      'python-aniso8601',
-      'python-tz',
-
-    # If using gunicorn
-    #'python-gunicorn',
-    #'gunicorn',
-    ##'python-gevent',  ## Debian = 1.0.1 , require == 1.0.2
-  ]
 
   if $burp::ui::manage_package {
 
     case $package_provider {
-
-      #
-      #  aptitude provider
-      #
-      'aptitude': {
-        # Work in progress.
-        package { $debian_packages :
-          ensure => present,
-        }
-      }
-
       #
       #  wheelhouse provider
       #
       'wheelhouse' : {
-        package { $debian_packages : ensure => present, }
 
         if $wheelhouse_source == undef {
           fail('You must provide your own path for wheelhouse_source')
@@ -55,7 +19,6 @@ class burp::ui::install (
         # NOTE: the wheelhouse was built with
         # pip wheel burp-ui
         ensure_packages('python-pip')
-        #ensure_packages('python-dev')
         file { $wheelhouse_path :
           source  => $wheelhouse_source,
           recurse => true,
@@ -64,28 +27,32 @@ class burp::ui::install (
           require => [
             File[$wheelhouse_path],
             Package['python-pip'],
-            Package[$debian_packages],
           ],
           command => "pip install --use-wheel --no-index --find-link=${wheelhouse_path} burp-ui",
           unless  => 'pip freeze | grep "burp-ui"',
+        }
+        exec { 'upgrade burp-ui' :
+          require => [
+            File[$wheelhouse_path],
+            Package['python-pip'],
+            Exec['install burp-ui'],
+          ],
+          command => "pip install --upgrade --use-wheel --no-index --find-link=${wheelhouse_path} burp-ui",
+          unless  => "pip freeze | grep 'burp-ui==${burpui_version}'",
         }
         exec { 'install ldap3' :
           require => [
             File[$wheelhouse_path],
             Package['python-pip'],
-            Package[$debian_packages],
           ],
           command => "pip install --use-wheel --no-index --find-link=${wheelhouse_path} ldap3",
           unless  => 'pip freeze | grep "ldap3"',
         }
 
-        # Require version 1.0.2 and 1.0.
         exec { 'install gevent' :
           require => [
             File[$wheelhouse_path],
             Package['python-pip'],
-            #Package['python-dev'],
-            Package[$debian_packages],
           ],
           command => "pip install --use-wheel --no-index --find-link=${wheelhouse_path} gevent",
           unless  => 'pip freeze | grep "gevent"',
