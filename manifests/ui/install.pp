41855e8143db2ff79486cfb2dbcfcde7226b9581
Patrick Brideau <pbrideau@kronostechnologies.com>
2016-08-08 14:29:36 -0400

    Refactor/simplify pip installation

--- manifests/ui/install.pp
+++ manifests/ui/install.pp
@@ -1,8 +1,10 @@
 class burp::ui::install (
-  $package_provider  = 'aptitude',
+  $package_provider  = 'wheelhouse',
   $wheelhouse_source = 'puppet:///modules/burp/ui-wheelhouse',
   $wheelhouse_path   = '/opt/burp-ui_wheelhouse',
   $burpui_version    = '0.2.1',
+  $ldap3_version     = '1.2.2',
+  $gevent_version    = '1.0.2',
 ) {
 
   if $burp::ui::manage_package {
@@ -18,45 +20,48 @@ class burp::ui::install (
         }
         # NOTE: the wheelhouse was built with
         # pip wheel burp-ui
-        ensure_packages('python-pip')
+        include ::python
         file { $wheelhouse_path :
           source  => $wheelhouse_source,
           recurse => true,
         }
+
+        case $::burp::ui::service_provider {
+          'gunicorn' : {
+            Exec['install burp-ui'] ~> Service['gunicorn']
+            Exec['install ldap3'] ~> Service['gunicorn']
+            Exec['install gevent'] ~> Service['gunicorn']
+          }
+          'builtin' : {
+            Exec['install burp-ui'] ~> Service['burp-ui']
+            Exec['install ldap3'] ~> Service['burp-ui']
+            Exec['install gevent'] ~> Service['burp-ui']
+          }
+        }
+
         exec { 'install burp-ui' :
           require => [
             File[$wheelhouse_path],
             Package['python-pip'],
           ],
-          command => "pip install --use-wheel --no-index --find-link=${wheelhouse_path} burp-ui",
-          unless  => 'pip freeze | grep "burp-ui"',
-        }
-        exec { 'upgrade burp-ui' :
-          require => [
-            File[$wheelhouse_path],
-            Package['python-pip'],
-            Exec['install burp-ui'],
-          ],
           command => "pip install --upgrade --use-wheel --no-index --find-link=${wheelhouse_path} burp-ui",
-          unless  => "pip freeze | grep 'burp-ui==${burpui_version}'",
-          notify  => Service['burp-ui'],
+          unless  => "pip show burp-ui | grep '^Version: ${burpui_version}'",
         }
         exec { 'install ldap3' :
           require => [
             File[$wheelhouse_path],
             Package['python-pip'],
           ],
-          command => "pip install --use-wheel --no-index --find-link=${wheelhouse_path} ldap3",
-          unless  => 'pip freeze | grep "ldap3"',
+          command => "pip install --upgrade --use-wheel --no-index --find-link=${wheelhouse_path} ldap3",
+          unless  => "pip show ldap3 | grep '^Version: ${ldap3_version}'",
         }
-
         exec { 'install gevent' :
           require => [
             File[$wheelhouse_path],
             Package['python-pip'],
           ],
-          command => "pip install --use-wheel --no-index --find-link=${wheelhouse_path} gevent",
-          unless  => 'pip freeze | grep "gevent"',
+          command => "pip install --upgrade --use-wheel --no-index --find-link=${wheelhouse_path} gevent",
+          unless  => "pip show gevent | grep '^Version: ${gevent_version}'",
         }
       }
 
